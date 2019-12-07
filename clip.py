e02f30b1da4e09e0aa0a41f7f06ac248846480d0
Tacio S. S <tacioss.br@gmail.com>
2019-12-07 14:57:25 -0300

    Create clip.py

---
+++ clip.py
@@ -0,0 +1,8 @@
+from firstbot import chatbot
+while True:
+    try:
+        bot_input = chatbot.get_response(input())
+        print(bot_input)
+
+    except(KeyboardInterrupt, EOFError, SystemExit):
+        break
